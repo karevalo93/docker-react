4847bc2d5f7e56742578a51f3d64d3cde357f740
karevalo93 <kevin.arevalo.l@hotmail.com>
2021-06-01 16:49:13 -0500

    added a new title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Fuck You!</p>
+        <p>El que no salte es tombo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
